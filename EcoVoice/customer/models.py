277c59973999489572bb4525e7b907776ef32fc5
SwapnilK0 <swapnilkale226@gmail.com>
2024-02-09 01:09:05 +0530

    some remaining migrations

--- EcoVoice/customer/models.py
+++ EcoVoice/customer/models.py
@@ -5,11 +5,11 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     
     # Additional fields for complaint filing
-    full_name = models.CharField(max_length=255, blank=True, null=True)
-    phone_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
-    city = models.CharField(max_length=20, blank=True, null=True)
-    state = models.CharField(max_length=20, blank=True, null=True)
-    zipcode = models.CharField(max_length=10, blank=True, null=True)
+    full_name = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=15, unique=True, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    state = models.CharField(max_length=20, blank=True)
+    zipcode = models.CharField(max_length=10, blank=True)
     
     USERNAME_FIELD ='email'
     REQUIRED_FIELDS =['email']
@@ -19,16 +19,16 @@ class CustomUser(AbstractUser):
 
 class Complaint(models.Model):
     id = models.IntegerField(unique=True,primary_key=True)
-    complint_name = models.CharField(max_length=100, blank=True, null=True)
-    crime_type = models.CharField(max_length=100, blank=True, null=True)
-    address = models.CharField(max_length=100, blank=True, null=True)
-    city = models.CharField(max_length=100, blank=True, null=True)
-    country = models.CharField(max_length=100, blank=True, null=True)
-    state = models.CharField(max_length=100, blank=True, null=True)
+    complint_name = models.CharField(max_length=100, blank=True)
+    crime_type = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
     zipcode = models.IntegerField()
     crime_date = models.DateField(auto_now=False, auto_now_add=False)
-    description = models.TextField(max_length=100, blank=True, null=True)
-    status = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(max_length=100, blank=True)
+    status = models.CharField(max_length=100, blank=True)
     
 
 
